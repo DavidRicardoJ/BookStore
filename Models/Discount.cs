a38071710ef7ef61dca5328966d44a1c45cbc8b0
José Ricardo David <david_Ricardo@live.com>
2021-09-16 23:07:50 -0300

    Add project files.

---
+++ Models/Discount.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Models
+{
+    public class Discount
+    {
+        public IEnumerable<Book> BookGroup { get; set; } // grupo de livros distintos que formam um grupo para desconto.
+        public double DiscountValue { get; set; } // valor da porcentagem de desconto.
+        public decimal Amount { get; set; } // valor do grupo com desconto.
+
+        
+
+        public List<List<Discount>> CalculateDiscount(List<Book> listBooks)
+        {
+            int distinctBooks = listBooks.Where(x => x.Title != null).Distinct().Count();
+            List<List<Discount>> DiscountList = new(); // varias combinações para achar o menor custo;
+
+
+            while (distinctBooks > 1)
+            {
+                DiscountList.Add(GetGroupsDiscount(listBooks, distinctBooks));
+                distinctBooks--;
+            }
+            return DiscountList;
+        }
+        public List<Discount> GetGroupsDiscount(List<Book> listBooks, int combination)
+        {
+            List<Discount> discountGroups = new();
+            bool groupExist = true;
+
+            while (groupExist)
+            {
+                BookGroup = DistinctBooks(listBooks, combination);
+
+                DiscountValue = GetDiscountGroup(BookGroup);
+
+                discountGroups.Add(new Discount { BookGroup = BookGroup, DiscountValue = DiscountValue });
+
+                RemoveDistinctBooksGroup(ref listBooks, BookGroup);
+
+                groupExist = VerifyGroupExist(listBooks);
+            }
+
+            GetAmountWithDiscount(discountGroups);
+
+            return discountGroups;
+        }
+
+        private IEnumerable<Book> DistinctBooks(List<Book> listBooks, int combination)
+        {
+            var distinctBooks = listBooks.Where(l => l.Quantity > 0).Distinct().Take(combination);
+            List<Book> books = new();            
+            foreach (var item in distinctBooks)
+            {
+                Book itemBook = new();
+                itemBook = item;
+                if (item.Quantity > 1)
+                {
+                    itemBook.Quantity = 1;
+                }
+                books.Add(itemBook);
+            }
+            return books;
+        }
+
+        private List<Book> RemoveDistinctBooksGroup(ref List<Book> listBooks, IEnumerable<Book> BookGroup)
+        {
+            List<Book> books = new();
+            var x = BookGroup.Select(s => s.Title).First();
+
+            foreach (var book in listBooks)
+            {
+                var newBook = book;
+                if (book.Quantity > 0 && BookGroup.Select(s => s.Title).Contains(book.Title))
+                {
+                    newBook.Quantity -= 1;
+                }
+                books.Add(newBook);
+            }
+           
+            listBooks = books;
+
+            return listBooks;
+        }
+
+        private double GetDiscountGroup(IEnumerable<Book> distinctBooks)
+        {
+            int numberBooks = distinctBooks.Count();
+            double discount = 0;
+
+            switch (numberBooks)
+            {
+                case 1:
+                    discount = 0;
+                    break;
+                case 2:
+                    discount = 5;
+                    break;
+                case 3:
+                    discount = 10;
+                    break;
+                case 4:
+                    discount = 20;
+                    break;
+                case 5:
+                    discount = 25;
+                    break;
+            }
+            return discount;
+        }
+
+        private IEnumerable<Discount> GetAmountWithDiscount(IEnumerable<Discount> discountGroups)
+        {
+            decimal sumAmount = new();
+            double discountPercentage = 0.0;
+            foreach (var group in discountGroups)
+            {
+                sumAmount = group.BookGroup.Sum(v => v.Price);
+                discountPercentage = group.DiscountValue;
+
+                group.Amount = sumAmount - (sumAmount * (decimal)discountPercentage / 100);
+            }
+            return discountGroups;
+        }
+
+        private bool VerifyGroupExist(List<Book> listBooks)
+        {
+            foreach (var item in listBooks)
+            {
+                if (item.Quantity > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // aqui vou simular a entrada de uma compra
+
+            var livro01 = new Book() { Title = "Livro 01", Price = new decimal(8.00), Quantity = 2 };
+            var livro02 = new Book() { Title = "Livro 02", Price = new decimal(8.00), Quantity = 2 };
+            var livro03 = new Book() { Title = "Livro 03", Price = new decimal(8.00), Quantity = 2 };
+            var livro04 = new Book() { Title = "Livro 04", Price = new decimal(8.00), Quantity = 1 };
+            var livro05 = new Book() { Title = "Livro 05", Price = new decimal(8.00), Quantity = 1 };
+
+            // criando uma lista ou array para armazenar os itens comprados.
+            List<Book> livros = new();
+            livros.Add(livro01);
+            livros.Add(livro02);
+            livros.Add(livro03);
+            livros.Add(livro04);
+            livros.Add(livro05);
+
+
+
+            Discount discount = new Discount();
+            List<List<Discount>> listGroupDiscount = new();
+            listGroupDiscount = discount.CalculateDiscount(livros);
+
+
+
+            PrintResult(GroupMinAmount(listGroupDiscount)); //Resultado
+
+          
+
+            static decimal MinAmountGroup(List<List<Discount>> listGroupDiscount)
+            {
+                List<decimal> AmountGroup = new();
+
+                foreach (var group in listGroupDiscount)
+                {
+                    var minAmount = group.Sum(x => x.Amount);
+                    AmountGroup.Add(minAmount);
+                }
+                return AmountGroup.Min();
+            }
+
+
+            static List<Discount> GroupMinAmount(List<List<Discount>> GroupDiscount)
+            {
+                var minAmount = MinAmountGroup(GroupDiscount);
+                List<Discount> AmountMinGroup = new();
+
+                foreach (var group in GroupDiscount)
+                {
+
+                    if (group.Sum(s=> s.Amount) == minAmount)
+                    {
+                       return group;
+                    }
+                }
+                return null;
+            }
+
+
+
+
+
+            static void PrintResult(List<Discount> GroupDiscount)
+            {
+                StringBuilder sb = new();
+                int groupNumber = 1;
+                decimal total = new();
+
+
+                sb.AppendLine("Livros comprados: \n");
+                foreach (var group in GroupDiscount )
+                {
+                    total += group.Amount;
+                    sb.AppendLine($"Grupo - {groupNumber}");
+                    var groupPrint = group.BookGroup.Select(s => new { titles = s.Title, price = s.Price, quantity = s.Quantity}).ToList();
+
+                    int groupPrintCount = groupPrint.Count;
+                  
+                    for (int i = 0; i < groupPrintCount; i++)
+                    {
+                        sb.Append($"Título: {groupPrint[i].titles} | ");
+                        sb.Append($"Quantidade: {groupPrint[i].quantity} | ");
+                        sb.AppendLine($"Preço: {groupPrint[i].price}");                       
+                    }
+                    sb.AppendLine();
+                    sb.AppendLine($"Percentual de desconto: {group.DiscountValue}%");
+                    sb.AppendLine($"Valor sem desconto: {group.BookGroup.Sum(s => s.Price).ToString("0.00")}");
+                    sb.AppendLine($"Valor com desconto: {group.Amount.ToString("0.00")}");
+                    sb.AppendLine("_______________________________________________\n");
+                    groupNumber++;
+                }
+
+                sb.AppendLine();
+                sb.AppendLine($"Valor total: {total.ToString("0.00")}");
+
+                Console.WriteLine(sb.ToString());
+            }
+
+           
+
+        }
+    }
+}
